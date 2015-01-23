146dfa652780092ba3b8d78c4477dda574125b0a
Simon Walker <s.r.walker101@googlemail.com>
2015-01-23 15:51:20 +0000

    run autopep8 over models test file

--- testing/test_models.py
+++ testing/test_models.py
@@ -1,10 +1,13 @@
 from k2catalogue import models
 
+
 def test_safe_float_good():
     assert models.safe_float("2") == 2.0
 
+
 def test_safe_float_bad():
     assert models.safe_float('this is not convertable to a float') is None
+
 
 def test_proposal_printing():
     proposal = models.Proposal(proposal_id='abc')

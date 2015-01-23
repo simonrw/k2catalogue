7087571ac19637fc017a3a543a476e73aef7b709
Simon Walker <s.r.walker101@googlemail.com>
2015-01-23 15:54:39 +0000

    Add test for proposal.create method

--- testing/test_models.py
+++ testing/test_models.py
@@ -1,3 +1,5 @@
+import pytest
+
 from k2catalogue import models
 
 
@@ -9,6 +11,16 @@ def test_safe_float_bad():
     assert models.safe_float('this is not convertable to a float') is None
 
 
-def test_proposal_printing():
-    proposal = models.Proposal(proposal_id='abc')
+@pytest.fixture
+def proposal():
+    return models.Proposal(proposal_id='abc')
+
+
+def test_proposal_printing(proposal):
     assert repr(proposal) == '<Proposal: abc>'
+
+
+def test_proposal():
+    proposals = models.Proposal.create(['abc', 'def'])
+    assert (proposals[0].proposal_id == 'abc' and
+            proposals[1].proposal_id == 'def')

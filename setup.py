7ab6e87a8cf89c12733f7923c0f4564672f549fc
Simon Walker <s.r.walker101@googlemail.com>
2015-01-23 15:31:56 +0000

    Add vcrpy back to main program requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='k2catalogue',
     version='0.0.1',
     author='Simon Walker',
-    install_requires=['requests', 'sqlalchemy', 'ipython',],
+    install_requires=['requests', 'sqlalchemy', 'ipython', 'vcrpy'],
     tests_require=['vcrpy', 'pytest'],
     packages=find_packages(exclude=['venv']),
     entry_points={

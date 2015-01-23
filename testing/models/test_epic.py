6876b6584cd90cca60fc21a53967dc1dfee6f2b4
Simon Walker <s.r.walker101@googlemail.com>
2015-01-23 16:02:13 +0000

    Add test for simbad query

--- testing/models/test_epic.py
+++ testing/models/test_epic.py
@@ -1,4 +1,8 @@
 import pytest
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 from k2catalogue import models
 
@@ -12,3 +16,8 @@ def epic():
 def test_repr(epic):
     assert repr(epic) == '<EPIC: 12345>'
 
+
+@mock.patch('k2catalogue.models.Simbad')
+def test_simbad_query(Simbad, epic):
+    epic.simbad_query(radius=2.)
+    Simbad.return_value.open.assert_called_once_with(radius=2.)

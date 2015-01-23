7109313773e0329b7b3562417d7daafe0b3e807c
Simon Walker <s.r.walker101@googlemail.com>
2015-01-23 14:53:16 +0000

    Add tox and make python2.7 compatible

--- testing/test_simbad.py
+++ testing/test_simbad.py
@@ -1,6 +1,9 @@
 import pytest
 import vcr
-from unittest import mock
+try:
+    from unittest import mock
+except ImportError:
+    import mock
 
 from k2catalogue.models import EPIC, create_session
 from k2catalogue.simbad import Simbad

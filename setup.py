477be188007e6895e652348afea7bb65f2d1c742
Simon Walker <s.r.walker101@googlemail.com>
2015-01-25 21:46:07 +0000

    Add beautifulsoup4 requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ setup(
     name='k2catalogue',
     version='0.0.2',
     author='Simon Walker',
-    install_requires=['requests', 'sqlalchemy', 'ipython', 'vcrpy'],
+    install_requires=['requests', 'sqlalchemy', 'ipython', 'vcrpy',
+                      'beautifulsoup4'],
     tests_require=['pytest'],
     packages=find_packages(exclude=['venv']),
     entry_points={

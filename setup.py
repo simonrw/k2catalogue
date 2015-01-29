52066a29d43d22dfe37a4a6b93c2e0380f7f2b47
Simon Walker <s.r.walker101@googlemail.com>
2015-01-29 10:11:05 +0000

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='k2catalogue',
-    version='0.3.0',
+    version='0.3.1',
     author='Simon Walker',
     install_requires=['requests', 'sqlalchemy', 'ipython', 'vcrpy',
                       'beautifulsoup4', 'lxml'],

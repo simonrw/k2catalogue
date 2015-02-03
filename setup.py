e9f2c586ad51c4efbaa69061a5da39f2cd60412b
Simon Walker <s.r.walker101@googlemail.com>
2015-02-03 13:40:35 +0000

    Add more setup.py metadata

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.realpath(path.dirname(__file__))
+
+with open(path.join(here, 'README.md'), encoding='utf-8') as infile:
+    long_description = infile.read()
 
 setup(
     name='k2catalogue',
@@ -6,10 +13,13 @@ setup(
     author='Simon Walker',
     author_email='s.r.walker101@googlemail.com',
     url='https://github.com/mindriot101/k2catalogue',
+    description='K2 Catalogue Queryier',
+    long_description=long_description,
+    license='MIT',
     install_requires=['requests', 'sqlalchemy', 'ipython', 'vcrpy',
                       'beautifulsoup4', 'lxml'],
     tests_require=['pytest'],
-    packages=find_packages(exclude=['venv']),
+    packages=find_packages(exclude=['venv', 'docs', 'testing']),
     entry_points={
         'console_scripts': [
             'k2cat-search = k2catalogue.cli:main',
@@ -27,5 +37,6 @@ setup(
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
-    ]
+    ],
+    keywords='astronomy kepler metadata',
 )
